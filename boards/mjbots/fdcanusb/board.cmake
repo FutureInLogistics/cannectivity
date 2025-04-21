6628d79bc50feb5012cb63c62b5467f084aa6f57
Igor Knippenberg <igor.knippenberg@gmail.com>
2025-04-22 01:18:31 +0200

    add fdcanusb

---
+++ boards/mjbots/fdcanusb/board.cmake
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: LicenseRef-Filics
+
+board_runner_args(jlink "--device=STM32G474CE" "--speed=4000" "--reset-after-load")
+board_runner_args(pyocd "--target=stm32g474cetx")
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
