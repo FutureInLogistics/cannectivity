2cc1fa52d6aaf8899f7b37fbc7869937ce4877b6
Igor Knippenberg <igor.knippenberg@gmail.com>
2025-04-23 05:34:37 +0200

    update fdcanusb board

--- boards/mjbots/fdcanusb/board.cmake
+++ boards/mjbots/fdcanusb/board.cmake
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: LicenseRef-Filics
 
+board_runner_args(openocd "--config=${BOARD_DIR}/support/openocd.cfg")
 board_runner_args(jlink "--device=STM32G474CE" "--speed=4000" "--reset-after-load")
 board_runner_args(pyocd "--target=stm32g474cetx")
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
